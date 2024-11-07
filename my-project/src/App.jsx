369b686e2ebfd844b5a7659e33ec537ea38d554f
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-11-08 00:50:21 +0500

    added all

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
 import "./App.css";
 import { AddToCard } from "./components/AddToCard";
-
-
+let AddToCardlist = [
+  { discount: "-40%", productName: "HAVIT HV-G92 Gamepad", delPrice: "$160",newPrice:"$120",starRank:"(88)" },
+  { discount: "-35%", productName: "AK-900 Wired Keyboard", delPrice: "$400",newPrice:"$370",starRank:"(99)" },
+  { discount: "-30%", productName: "IPS LCD Gaming Monitor", delPrice: "$400",newPrice:"$350",starRank:"(77)" },
+  { discount: "-25%", productName: "S-Series Comfort Chair", delPrice: "$400",newPrice:"$375",starRank:"(99)" },
+];
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-  <AddToCard/>
+    {
+      AddToCardlist.map((value ,index)=>{
+        return(
+          <AddToCard id={index}  discount={value.discount} productName={value.productName} delPrice={value.delPrice} newPrice={value.newPrice} starRank={value.starRank}/>
+
+        )
+      })
+    }
     </>
   );
 }
--- my-project/src/components/AddToCard.jsx
+++ my-project/src/components/AddToCard.jsx
@@ -1,18 +1,34 @@
 import game from "../assets/images/game.png";
+import star from "../assets/images/Five star.png";
 
-export function AddToCard() {
+export function AddToCard({
+  discount,
+  productName,
+  delPrice,
+  newPrice,
+  starRank,
+}) {
   return (
     <>
-      <div className="main bg-slate-500  w-[270px] h-[350px] m-8 ">
-        <div className="img w-[270px] h-[250px] bg-slate-200 relative ">
-        <button
+      <div className="main  bg-white  w-[270px] h-[350px] m-8 ">
+        <div className="img w-[270px] h-[250px] bg-[#F5F5F5] relative center ">
+          <button
             className=" w-[55px] h-[26px] bg-red-500 rounded text-white absolute left-3 right-4 bottom-1 top-3 ;
 "
           >
-            -40%
+            {discount}
           </button>
-          <img className=" w-[190px] h-[180px] m-10" src={game} alt="" />
-          
+          <img className=" w-[172px] h-[152px] m-10 hover:scale-125" src={game} alt="" />
+        </div>
+        <div className="myp w-[201px] h-[84px] gap-2 my-3">
+          <h2 className="w-[201] h-6 text-base font-medium hover:text-[#DB4444]">{productName}</h2>
+          <p className="flex gap-4 my-2 w-[100px] h-6  text-base font-medium text-[#DB4444]">
+            <ins className="no-underline"> {newPrice}</ins> {"  "}
+            <s className="text-slate-400 "> {delPrice}</s>
+          </p>
+          <div className="star-img flex gap-2">
+            <img src={star} alt="" /><span>{starRank}</span>
+          </div>
         </div>
       </div>
     </>
