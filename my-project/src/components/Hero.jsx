d9d2c2996134c84c3a6ba62b6e04e848e84b2016
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-11-09 11:11:34 +0500

    update

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -1,31 +1,38 @@
-
-let heroLink=[
-    {name:"Woman's Fashion"},
-    {name:"Men's Fashion"},
-    {name:"Electronics"},
-    {name:"Home & StyleLife"},
-    {name:"Medicine"},
-    {name:"Sports & Outdoor"},
-    {name:"Baby's & Toys"},
-    {name:"Groceries & Pets"},
-    {name:"Health & Beauty"},
-
-]
+let heroLink = [
+  { name: "Woman's Fashion" },
+  { name: "Men's Fashion" },
+  { name: "Electronics" },
+  { name: "Home & StyleLife" },
+  { name: "Medicine" },
+  { name: "Sports & Outdoor" },
+  { name: "Baby's & Toys" },
+  { name: "Groceries & Pets" },
+  { name: "Health & Beauty" },
+];
 
 export default function Hero() {
- return (<>
- 
- <div className="heroMain">
-    <div className="lefthero bg-orange-700 border-r-4 border-black"><div className="liHero w-[270px] h-[344px] pl-[135px] gap-4 bg-slate-400 ">
-        {heroLink.map((value , index)=>{
-            return(
-<li className="list-none" id={index}>{value.name}</li>
-            )
-        })}
+  return (
+    <>
+      <div className="heroMain flex">
+        <div className="lefthero   gap-4  border-r-2 border-gray-100 w-[310px]">
+          <div className="liHero   pl-12 ml-9 w-[270px] h-[344px]  gap-4   ">
+           <div className="  liPadding w-[217px] h-[340px]   mt-12"> 
+            {heroLink.map((value, index) => {
+              return (
+                <li className=" list-none pl-3 w-[217px] h-[24px] gap-[81px] mb-[15px] font-normal text-base " id={index}>
+                  {value.name}
+                </li>
+              );
+            })}
+            </div>
+          </div>
         </div>
-    </div>
- </div>
- 
- 
- </>)
+        <div className="heroright ">
+            <div className="heroCan ml-[45px] w-[892px] h-[334px] bg-black mt-12">
+
+            </div>
+        </div>
+      </div>
+    </>
+  );
 }
