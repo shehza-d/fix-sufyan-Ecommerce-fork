9c07e2940a5945b94869b552a92d080365e7fa3b
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-11-18 01:50:06 +0500

    color

--- my-project/src/Pages/Contact.jsx
+++ my-project/src/Pages/Contact.jsx
@@ -4,7 +4,7 @@ import location from "../assets/profilepic/location.jpg"
 
 function Contact() {
   return (
-    <section className="text-gray-600 body-font relative">
+    <section className="text-black body-font relative">
   <div className="container px-5 py-24 mx-auto flex  sm:flex-nowrap flex-wrap">
     <div className="lg:w-2/3 md:w-1/2   overflow-hidden sm:mr-10 p-10 flex flex-col items-center justify-start relative">
       {/* <iframe
@@ -43,14 +43,14 @@ function Contact() {
       </div> */}
     </div>
     <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-      <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+      <h2 className="text-gray-900 text-2xl mb-1 font-bold title-font">
         Feedback
       </h2>
-      <p className="leading-relaxed mb-5 text-gray-600">
+      <p className="leading-relaxed mb-5 text-black">
         We Need For Your Feedback
       </p>
       <div className="relative mb-4">
-        <label htmlFor="name" className="leading-7 text-sm text-gray-600">
+        <label htmlFor="name" className="leading-7 text-sm text-black">
           Name
         </label>
         <input
@@ -61,7 +61,7 @@ function Contact() {
         />
       </div>
       <div className="relative mb-4">
-        <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+        <label htmlFor="email" className="leading-7 text-sm text-black">
           Email
         </label>
         <input
@@ -72,7 +72,7 @@ function Contact() {
         />
       </div>
       <div className="relative mb-4">
-        <label htmlFor="message" className="leading-7 text-sm text-gray-600">
+        <label htmlFor="message" className="leading-7 text-sm text-black">
           Message
         </label>
         <textarea
@@ -85,7 +85,7 @@ function Contact() {
       <button className="text-white bg-black border-0 py-2 px-6 focus:outline-none active:bg-myTheme  text-lg">
         Button
       </button>
-      <p className="text-xs text-gray-500 mt-3">
+      <p className="text-xs text-black mt-3">
        The Knowldge is Light
       </p>
     </div>
