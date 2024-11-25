addc2e3d8234605d807219e64e4d64a2f480ef09
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-11-25 13:17:11 +0500

    update add filter

--- my-project/src/Pages/AllProduct.jsx
+++ my-project/src/Pages/AllProduct.jsx
@@ -147,12 +147,60 @@ function AllProduct() {
     getProductData();
 
   },[])
+       
+   const [searchValue,setSearchValue]=useState("")
+   console.log(searchValue);
+
+   const searchProducts=()=>{
+    let result=  products?.filter((value)=>{
+    return value.price<100
+    })
+    console.log(result);
+    
+       }
+   searchProducts()
 
   return (
     // <div>AllProduct</div>
     <>
-     
-      <div className="flex justify-center items-center   my-14  flex-row">{products===null?<div className="flex justify-center h-[100vh] text-center items-center font-extrabold text-lg">Please Wait...</div>:null}
+          <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+  <div className="relative isolate overflow-hidden bg-white px-6 py-20 text-center sm:px-16 sm:shadow-sm">
+    <p className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+      Search Products
+    </p>
+    <form >
+      <label
+        className="mx-auto mt-8 relative bg-white min-w-sm max-w-2xl flex flex-col md:flex-row items-center justify-center border py-2 px-2 rounded gap-2 shadow-2xl focus-within:border-gray-300"
+        htmlFor="search-bar"
+      >
+        <input
+          id="search-bar"
+          placeholder="Search your Products"
+          name="q"
+          onChange={(event)=>{
+            // console.log(event.target.value);
+            setSearchValue(event.target.value)
+            
+          }}
+          className="px-6 py-2 w-full rounded flex-1 outline-none bg-white"
+          required=""
+        />
+        <button
+          type="submit"
+          className="w-full md:w-auto px-6 py-3 bg-black border-black text-white fill-white active:scale-95 duration-100 border will-change-transform overflow-hidden relative rounded transition-all"
+        >
+          <div className="flex items-center transition-all opacity-1">
+            <span className="text-sm font-semibold whitespace-nowrap truncate mx-auto">
+              Search
+            </span>
+          </div>
+        </button>
+      </label>
+    </form>
+
+  </div>
+</div>
+      <div className="flex justify-center items-center   my-14  flex-row">{products===null?<div className="flex justify-center h-[100vh] w-full text-center items-center font-extrabold text-lg">Please Wait...</div>:null}
         <div  className=" flex sm:flex-col  w-[1170px]  flex-wrap mobile:flex-col lg:justify-center md:justify-center md:flex-row 2xl:flex-row xl:flex-row lg:flex-row lg:gap-4 gap-[30px]">
           {products?.map((value, index) => {
             let discountPrice=Math.ceil(value.price-(value.discountPercentage)*(value.price/100)).toFixed(2)
@@ -169,6 +217,7 @@ function AllProduct() {
                   delPrice={`$${value.price.toFixed(2)}`}
                   newPrice={`$${discountPrice}`}
                   image={value.thumbnail}
+                  rating={value.rating}
                 />
               </>
             );
--- my-project/src/components/AddToCard.jsx
+++ my-project/src/components/AddToCard.jsx
@@ -1,6 +1,7 @@
 import game from "../assets/images/game.png";
 import star from "../assets/images/Five star.png";
 import { Link } from "react-router-dom";
+import ReactStars from 'react-stars'
 
 
 
@@ -16,12 +17,13 @@ export function AddToCard({
   className,
   deleticon,
   hearticon,
-  eyeicon
+  eyeicon,
+  rating
 }) {
   return (
     <>
 
-      <div id={key} className="main  bg-white   w-[270px] h-[350px] my-4 rounded mobile:m-auto ">
+      <div id={key} className="main  bg-white drop-shadow-lg shadow-black   w-[270px] h-[370px] my-4 rounded mobile:m-auto ">
         <div className="img w-[270px] h-[250px] bg-[#F5F5F5] flex flex-col relative center ">
           <button
             className={` w-[55px] h-[26px] ${className} rounded text-white absolute left-3 right-4 bottom-1 top-3 ;`}
@@ -31,18 +33,26 @@ export function AddToCard({
         { hearticon? <div className="absolute top-2 left-[240px] text-base bg-white p-1 rounded-full"> {hearticon}</div>:null}
       { deleticon?   <div className="absolute top-2 left-[240px] text-base bg-white p-1 rounded-full">{deleticon}</div>:null}
         { eyeicon? <div className="absolute top-9 left-[240px] text-base bg-white p-1 rounded-full"> {eyeicon}</div>:null}
-          </div>
-          <img className=" w-[172px] h-[155px] m-auto hover:scale-125" src={image} alt="" />
+          </div>{image?
+          <img className=" w-[172px] h-[155px] m-auto hover:scale-125" src={image} alt="" />:<div className=" w-[172px] h-[155px]"></div>}
           <Link  className="bg-white hover:bg-black h text-white w-[100%] flex  items-end justify-center p-2 rounded-b">Add To Card</Link>
         </div>
-        <div className="myp w-[201px] h-[84px] ml-2 gap-2 my-3">
+        <div className="myp w-[201px] h-[84px] ml-2 gap-2 ">
           <h2 className="w-[201]  text-sm font-medium hover:text-myTheme">{productName}</h2>
           <p className="flex gap-4 my-2 w-[100px] h-6  text-base font-medium text-myTheme">
             <ins className="no-underline"> {newPrice}</ins> {"  "}
             <s className="text-slate-400 "> {delPrice}</s>
           </p>
           <div className="star-img flex gap-2">
-            <img src={star} alt="" /><span>{starRank}</span>
+            <span>{
+                        <ReactStars
+                        count={5}
+                        value={rating?rating:3 }
+                        // onChange={ratingChanged}
+                        size={24}
+                        edit={false}
+                        color2={'#ffd700'} />
+            }</span><span>{  }</span>
           </div>
         </div>
       </div>
